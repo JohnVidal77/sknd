34be801863ee16cfc2b23b43c4f02eb5542e33e4
John Vidal <ba.john.vidal@gmail.com>
2020-09-19 01:57:45 -0300

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # SKND - Alpha
+
+Basic project using just HTML, JS and CSS. No frameworks and libraries.
+
+## Roadmap - alpha-v1
+
+- Create a basic users list with Github API
+- Remove users from this list
+- Redirect to user's gh page on click
+
+## Main Goals
+
+- Create basic social network
+- Create a backend
+- PWA
--- index.js
+++ index.js
@@ -5,6 +5,12 @@ document.getElementById("form").addEventListener("submit", function (e) {
 
   fetch(`https://api.github.com/users/${inputValue}`)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      let apiResponse = data;
+
+      document.getElementById(
+        "result"
+      ).innerHTML = `<h1>${apiResponse.name}</h1>`;
+    })
     .catch((err) => console.log(err));
 });
